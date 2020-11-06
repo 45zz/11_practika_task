29b7b6f32d549b588a4f57f98044f6937321613f
45zz <bugro.home@gmail.com>
2020-11-06 12:53:44 +0300

    Initial commit

---
+++ src/com/company/ListElement.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class ListElement {
+    public ListElement next; //следующий в очереди
+    public int element;
+
+    public ListElement getNext() {
+        return next;
+    }
+
+    public int getElement() {
+        return element;
+    }
+
+    @Override
+    public String toString() {
+        return "ListElement{" +
+                "next=" + next +
+                ", element=" + element +
+                '}';
+    }
+}
